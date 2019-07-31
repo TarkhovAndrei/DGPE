37ffd29b9042429197f70166e5611d46ae051324
TarkhovAndrei <tarkhov.andrey@gmail.com>
2019-07-31 16:13:48 +0300

    Intermediate

--- GPElib/dynamics_generator.py
+++ GPElib/dynamics_generator.py
@@ -676,14 +676,18 @@ class DynamicsGenerator(object):
 		return -self.quenching_gamma * 1./(self.lam1-self.lam2) * (self.lam1 * np.exp(-self.lam1 * time) - self.lam2 * np.exp(-self.lam2 * time))
 
 	def quenching_profile_to_room(self, psi, time=0.):
-		if time <= self.quenching_extremum_time:
-			self.quenching_extremum_energy = self.calc_energy_XY(psi[:self.N_wells],psi[self.N_wells:],0)
-			return self.quenching_profile(time)
-		else:
-			return ((self.quenching_profile(time) +
-					self.gamma * (1. - np.exp(-self.lam2 * (time - self.quenching_extremum_time))))
-				   * 1./ (self.E_desired)
-				   * (self.calc_energy_XY(psi[:self.N_wells],psi[self.N_wells:],0) - self.E_desired))
+		# if time <= self.quenching_extremum_time:
+		# 	self.quenching_extremum_energy = self.calc_energy_XY(psi[:self.N_wells],psi[self.N_wells:],0)
+		# 	return self.quenching_profile(time)
+		# else:
+		# 	return ((self.quenching_profile(time) +
+		# 			self.gamma * (1. - np.exp(-self.lam2 * (time - self.quenching_extremum_time))))
+		# 		   * 1./ (self.E_desired)
+		# 		   * (self.calc_energy_XY(psi[:self.N_wells],psi[self.N_wells:],0) - self.E_desired))
+		return (self.quenching_profile(time) +
+				(self.gamma * (1. - np.exp(-self.lam2 * (time - self.quenching_extremum_time))))
+				 * 1. / (self.E_desired)
+				 * (self.calc_energy_XY(psi[:self.N_wells], psi[self.N_wells:], 0) - self.E_desired))
 
 	def get_gamma_reduction(self, psi, time=0.):
 		if self.temperature_dependent_rate:
